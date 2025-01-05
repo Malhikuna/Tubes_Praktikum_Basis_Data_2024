e76acef91e638a17133d784aa9d6e94b8531943b
falen23 <byzad252@gmail.com>
2025-01-05 18:07:54 +0700

    ooo

---
+++ Insert_Alamat.sql
@@ -0,0 +1,19 @@
+INSERT INTO Alamat (Alamat_Id, Status_Keanggotaan_Id, Divisi_Id, Kecamatan, Kabupaten, Provinsi, Kode_Pos)
+VALUES (1, 1, 1, 'Kecamatan1', 'Kabupaten1', 'Provinsi1', '12345');
+
+INSERT INTO Alamat (Alamat_Id, Status_Keanggotaan_Id, Divisi_Id, Kecamatan, Kabupaten, Provinsi, Kode_Pos)
+VALUES (2, 2, 2, 'Kecamatan2', 'Kabupaten2', 'Provinsi2', '54321');
+
+-- Tambahkan 58 query lainnya di sini
+INSERT INTO Alamat (Alamat_Id, Status_Keanggotaan_Id, Divisi_Id, Kecamatan, Kabupaten, Provinsi, Kode_Pos)
+VALUES (3, 1, 3, 'Kecamatan3', 'Kabupaten3', 'Provinsi3', '67890');
+
+INSERT INTO Alamat (Alamat_Id, Status_Keanggotaan_Id, Divisi_Id, Kecamatan, Kabupaten, Provinsi, Kode_Pos)
+VALUES (4, 2, 4, 'Kecamatan4', 'Kabupaten4', 'Provinsi4', '09876');
+
+-- Lanjutkan pola ini hingga mencapai total 60 query
+INSERT INTO Alamat (Alamat_Id, Status_Keanggotaan_Id, Divisi_Id, Kecamatan, Kabupaten, Provinsi, Kode_Pos)
+VALUES (5, 1, 5, 'Kecamatan5', 'Kabupaten5', 'Provinsi5', '11223');
+
+INSERT INTO Alamat (Alamat_Id, Status_Keanggotaan_Id, Divisi_Id, Kecamatan, Kabupaten, Provinsi, Kode_Pos)
+VALUES (6, 2, 6, 'Kecamatan6', 'Kabupaten6', 'Provinsi6', '33445');
